9a1cad09c3113418147b746b4922b3ff122c003e
thtuan <thtuan1995@gmail.com>
2018-10-17 23:19:51 +0700

    Initial Commit

---
+++ FunCompass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunCompass
+//
+//  Created by thanh tuan on 10/17/18.
+//  Copyright © 2018 HeFun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

48fee7d87e2ce27ae7c3b330d8d32dfd970bf046
thtuan <thtuan1995@gmail.com>
2018-10-18 22:11:16 +0700

    push updated

--- FunCompass/MapViewController.swift
+++ FunCompass/MapViewController.swift
@@ -7,3 +7,102 @@
 //
 
 import Foundation
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+class MapViewController: UIViewController, GMSMapViewDelegate {
+    
+    var latlngUpdated: ((_ lat: Double, _ lng: Double) ->())?
+    var mapView: GMSMapView!
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var zoomLevel: Float = 15.0
+    var centerMapCoordinate:CLLocationCoordinate2D!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+    }
+    override func loadView() {
+        mapView = GMSMapView(frame: CGRect.zero)
+        mapView.delegate = self
+        view = mapView
+//        mapView.settings.myLocationButton = true
+    }
+    
+    func goToPlace(lat: Double, lng: Double){
+        let camera = GMSCameraPosition.camera(withLatitude: lat, longitude: lng, zoom: zoomLevel)
+        mapView.animate(to: camera)
+        view.endEditing(true)
+    }
+    
+    func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
+        let latitude = mapView.camera.target.latitude
+        let longitude = mapView.camera.target.longitude
+        centerMapCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        if((latlngUpdated) != nil){
+            latlngUpdated!(latitude, longitude)
+        }
+//        self.placeMarkerOnCenter(centerMapCoordinate:centerMapCoordinate)
+    }
+    
+    func placeMarkerOnCenter(centerMapCoordinate:CLLocationCoordinate2D) {
+        let marker = GMSMarker()
+        marker.position = centerMapCoordinate
+        marker.map = self.mapView
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    // Handle incoming location events.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location: \(location)")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+        manager.stopUpdatingLocation()
+    }
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        }
+    }
+    
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
+    }
+    
+}
+
+
--- FunCompass/ViewController.swift
+++ FunCompass/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import GoogleMaps
+
 
 class ViewController: UIViewController {
-
+    weak var containerViewController: MapViewController!
+    
+    @IBOutlet weak var latInput: UITextField!
+    @IBOutlet weak var lngInPut: UITextField!
+    
+    @IBAction func goToPlace(_ sender: UIButton) {
+        let lat = Double(latInput.text!)
+        let lng = Double(lngInPut.text!)
+        containerViewController.goToPlace(lat: lat!, lng: lng!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.hideKeyboard()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let controller = segue.destination as? MapViewController, segue.identifier == "MapViewController" {
+            containerViewController = controller;
+            containerViewController.latlngUpdated = {lat, lng in
+                self.latInput.text = String(format:"%f", lat)
+                self.lngInPut.text = String(format:"%f", lng)
+            }
+        }
     }
-
-
 }
 
+extension UIViewController
+{
+    func hideKeyboard()
+    {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(UIViewController.dismissKeyboard))
+        
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard()
+    {
+        view.endEditing(true)
+    }
+}
